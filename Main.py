e9255eb1cdafdcdd42fc2bf8f9b1c48889e47e69
RadioactiveHaze <jonemo@stud.ntnu.no>
2019-03-27 22:30:04 +0100

    finished init of matrix

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
+
+#init matrix
 matrix = []
 matrixpart = []
-x = int(input("X: "));
-y = int(input("Y: "));
-for i in range(0, y):
-	
+x = int(input("X: "))
+y = int(input("Y: "))
+userinput = ""
+for i in range(0, y+1):
+	matrixpart = []
+	while len(userinput) != y:
+		print "x="+str(i)
+		userinput = str(raw_input("enter entire x: "))
+		print(len(userinput))
+	print userinput
+	for part in userinput:
+		matrixpart.append(int(part))
+	matrix.append(matrixpart)
+	userinput = ""
 
